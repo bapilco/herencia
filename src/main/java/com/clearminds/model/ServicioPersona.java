950a850a7b67f01bd0f08f6f95dab841f7d55d7e
bapilco <bryanpilco.1597@gmail.com>
2020-12-10 10:19:05 -0500

    Version2.0

---
+++ src/main/java/com/clearminds/model/ServicioPersona.java
@@ -0,0 +1,10 @@
+package com.clearminds.model;
+
+public interface ServicioPersona {
+	
+	public boolean insertar (Persona persona);
+	public boolean actualizar (Persona persona);
+	public boolean eliminar (Persona persona);
+	
+
+}
---
+++ src/main/java/com/clearminds/model/ServicioPersonaBDD.java
@@ -0,0 +1,20 @@
+package com.clearminds.model;
+
+public class ServicioPersonaBDD implements ServicioPersona{
+
+	public boolean insertar(Persona persona) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public boolean actualizar(Persona persona) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public boolean eliminar(Persona persona) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
