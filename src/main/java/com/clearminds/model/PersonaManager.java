0b21674e6fff99b05f9acb128d696cf7692f837e
bapilco <bryanpilco.1597@gmail.com>
2020-12-10 11:32:55 -0500

    version final

--- src/main/java/com/clearminds/model/PersonaManager.java
+++ src/main/java/com/clearminds/model/PersonaManager.java
@@ -1,34 +1,55 @@
 package com.clearminds.model;
 
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 public class PersonaManager {
-	
+
 	private ServicioPersona serv;
-	
-	public PersonaManager() throws InstanceException  {
+
+	public PersonaManager() throws InstanceException {
 		Class<?> clase = null;
 		try {
-			clase = Class.forName("com.clearminds.model.ServicioPersonaArchivo");
-			//throw new ClassException("Con que permiso????????");
+			String prop = propiedad();
+			clase = Class.forName("com.clearminds.model."+prop);
+			// throw new ClassException("Con que permiso????????");
 		} catch (ClassNotFoundException e) {
-			//e.printStackTrace();
+			// e.printStackTrace();
 			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
 		}
 		try {
-			serv=(ServicioPersona)clase.newInstance();
+			serv = (ServicioPersona) clase.newInstance();
 		} catch (InstantiationException e) {
 			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			// e.printStackTrace();
 			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
 		} catch (IllegalAccessException e) {
 			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			// e.printStackTrace();
 			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
 		}
 	}
-	
-	public void insertarPersona(Persona persona){
+
+	public String propiedad() {
+		try {
+			Properties p = new Properties();
+			p.load(new FileReader(
+					"D:/BRYAN/CAPACITACIONCMC/ProyectosGIT/herencia/src/main/java/properties/config.properties"));
+			String prop = p.getProperty("implementacion");
+			return prop;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	public void insertarPersona(Persona persona) {
 		serv.insertar(persona);
 	}
 
